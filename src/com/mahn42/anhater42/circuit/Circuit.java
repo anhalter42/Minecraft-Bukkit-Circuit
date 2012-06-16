88b2a36c3cad479fa213bbccc9dd59286d9b42d2
Andre Schloemp <andre.schloemp@me.com>
2012-06-17 00:04:08 +0200

    - initial

---
+++ src/com/mahn42/anhater42/circuit/Circuit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.anhater42.circuit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author andre
+ */
+public class Circuit extends JavaPlugin {
+
+    public static void main(String[] args) {
+    }
+}
