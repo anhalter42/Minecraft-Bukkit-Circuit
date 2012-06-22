45ba09c6dc83907cca1256ed3a1ca4db3d0c7a4f
Andre Schloemp <andre.schloemp@me.com>
2012-06-22 20:11:22 +0200

    - rename anhater42 -> anhalter42

--- src/com/mahn42/anhalter42/circuit/CircuitHandlerContext.java
+++ src/com/mahn42/anhalter42/circuit/CircuitHandlerContext.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mahn42.anhater42.circuit;
+package com.mahn42.anhalter42.circuit;
 
 import com.mahn42.framework.SyncBlockList;
 import java.util.HashMap;
