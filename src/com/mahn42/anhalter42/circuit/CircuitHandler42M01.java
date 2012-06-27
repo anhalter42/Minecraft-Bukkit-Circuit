abbf0882e3dcea79b84d03f5ddd490a0ce57ce3b
Andre Schloemp <andre.schloemp@me.com>
2012-06-27 16:01:32 +0200

    - 42M01 added

---
+++ src/com/mahn42/anhalter42/circuit/CircuitHandler42M01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.anhalter42.circuit;
+
+/**
+ *
+ * @author andre
+ */
+public class CircuitHandler42M01 extends CircuitHandler {
+    public CircuitHandler42M01() {
+        super("DIP4", "42M01");
+        pins.add(PinMode.Input);
+        pins.add(PinMode.Input);
+        pins.add(PinMode.Output);
+        pins.add(PinMode.Output);
+    }
+    
+    @Override
+    protected void tick() {
+        setPin("pin3", fContext.pins.get("pin1").newValue | fContext.pins.get("pin2").newValue);
+        setPin("pin4", !(fContext.pins.get("pin1").newValue | fContext.pins.get("pin2").newValue));
+    }
+}
