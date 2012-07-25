3ecc30da66c3cbf23179c4444d9e6f29ab264abf
Andre Schloemp <andre.schloemp@me.com>
2012-07-25 22:35:45 +0200

    - 42M2003 has now custom chars for the corresponing input code

--- src/com/mahn42/anhalter42/circuit/CircuitHandler42M2003.java
+++ src/com/mahn42/anhalter42/circuit/CircuitHandler42M2003.java
@@ -26,7 +26,17 @@ public class CircuitHandler42M2003 extends CircuitHandler {
                 | (getPinValueInt("pin2") << 1)
                 | (getPinValueInt("pin3") << 2)
                 | (getPinValueInt("pin4") << 3);
-        CircuitFont.CircuitFontChar lChar = Circuit.plugin.configASCI_3.getChar(lCode);
+        CircuitFont.CircuitFontChar lChar;
+        if (fContext.circuit.signLine2 != null && !fContext.circuit.signLine2.isEmpty()) {
+            if (fContext.circuit.signLine2.length() > lCode) {
+                char lC = fContext.circuit.signLine2.charAt(lCode);
+                lChar = Circuit.plugin.configASCI_3.getChar("" + lC);
+            } else {
+                lChar = Circuit.plugin.configASCI_3.getChar(" ");
+            }
+        } else {
+            lChar = Circuit.plugin.configASCI_3.getChar(lCode);
+        }
         for(int lX = 1; lX <= 3; lX++) {
             for(int lY = 1; lY <= 3; lY++) {
                 setPinInArea("out", lX, lY, lChar.getPixel(lX - 1, 3 - lY));
